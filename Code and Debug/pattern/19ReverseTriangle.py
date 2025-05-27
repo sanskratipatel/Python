f5f567844704d635e5f64405a5b8cebf972a7472
Sanskrati Patel <patelsanskrati@gmail.com>
2025-05-28 00:55:18 +0530

    Patterns Question

---
+++ Code and Debug/pattern/19ReverseTriangle.py
@@ -0,0 +1,23 @@
+# 6 6 6 6 6 6 
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+for i in range(6,0,-1):
+    for j in range(i,0,-1):
+        print(i,end=" ")
+    print() 
+
+
+# 6 5 4 3 2 1
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+for i in range(6,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
